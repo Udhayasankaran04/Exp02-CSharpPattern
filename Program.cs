e58b9d1d11c7b2f891b2db79bb1c7751f9908797
Udhaya sankaran M <119393933+Udhayasankaran04@users.noreply.github.com>
2024-05-11 19:06:59 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
-﻿
 using System;
 public class Pattern
 {
     public static void Main()
     {
-        // type the code here
+    int r=Convert.ToInt32(Console.ReadLine());
+    for(int i=0;i<r;i++)
+    {
+        for(int s=0;s<r-i;s++)
+        {
+            Console.Write(" ")
+            for( int j=0; j<i;j++)
+            {
+                if(i==0||j==0)
+                {
+                    c=1;
+                }
+                else
+                {
+                    c=c*(i-j+1)/i;
+                    Console.Write(c+" ");
+                }
+            }
+
+        }
+    }   
     }
 }
